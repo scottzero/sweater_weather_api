84fd78a949c1e3677c0a7fdcfc29a75c6dd10f6c
Scott Payton <spayton.c@gmail.com>
2019-11-04 12:00:16 -0700

    started refactor, still have desired user story output but broke my test

--- app/controllers/api/v1/antipodes_controller.rb
+++ app/controllers/api/v1/antipodes_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::AntipodesController < ApplicationController
   def show
     antipode_data = AntipodesFacade.new(params[:location])
     serialize_me = antipode_data.get_forecast
-    binding.pry
     render json: AntipodesSerializer.new(serialize_me)
   end
 end
--- app/facades/antipodes_facade.rb
+++ app/facades/antipodes_facade.rb
@@ -6,18 +6,7 @@ class AntipodesFacade
   end
 
   def get_forecast
-    #get coords for hong kong
-    geocode_service = GeocodingService.new(@location)
-    #format hong kong coords
-    coordinates = geocode_service.lat_long_coords
-    #pass coords to antiposed service
-    antipodes_service = AntipodesService.new(coordinates)
-    coordinates_for_antipode = antipodes_service.format_coords
-    x = antipodes_service.antipode_response
-    # {:data=>{:id=>"1", :type=>"antipode", :attributes=>{:lat=>-22.3193039, :long=>-65.8306389}}}
-    # x[:data][:attributes] <---- antipode city coords
-    # binding.pry
-    darksky_service = DarkskyService.new(x[:data][:attributes])
+    darksky_service = DarkskyService.new(coordinate_handler[:data][:attributes])
     current_antipode_forecast = darksky_service.parsed_darksky_response_data
     reverse_look = ReverseLookupService.new(current_antipode_forecast)
     antipode_city = reverse_look.parsed_google_geocode_data
@@ -26,5 +15,13 @@ class AntipodesFacade
     forecast_summary = current_antipode_forecast[:currently][:summary]
     antipode_temp = current_antipode_forecast[:currently].values[6]
     AntipodesPoro.new(antipode_name, "antipode",forecast_summary,antipode_temp)
+  end
+
+  def coordinate_handler
+    geocode_service = GeocodingService.new(@location)
+    coordinates = geocode_service.lat_long_coords
+    antipodes_service = AntipodesService.new(coordinates)
+    coordinates_for_antipode = antipodes_service.format_coords
+    antipodes_service.antipode_response
   end
 end
