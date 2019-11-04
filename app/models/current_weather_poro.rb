5ed01cd54a49b60243a80eac201e99c5a066a4f3
Scott Payton <spayton.c@gmail.com>
2019-11-03 22:37:01 -0700

    static fix to nil data hash values

--- app/models/current_weather_poro.rb
+++ app/models/current_weather_poro.rb
@@ -2,15 +2,15 @@ class CurrentWeatherPoro
   attr_reader :time, :summary, :icon, :tempurature, :humididity,
   :uv_index, :visibility, :feels_like
 
-  def initialize(current_data)
-    @time = current_data[:time]
-    @summary = current_data[:summary]
-    @icon = current_data[:icon]
-    @tempurature = current_data[:tempurature]
-    @humidity = current_data[:humidity]
-    @uv_index = current_data[:uv_index]
-    @visibility = current_data[:visibility]
-    @feels_like = current_data[:feels_like]
+  def initialize(data)
+    binding.pry
+    @time = data[:time]
+    @summary = data[:summary]
+    @icon = data[:icon]
+    @tempurature = data.values[7] #keeps coming back nil here for some reason
+    @humidity = data[:humidity]
+    @uv_index = data.values[16]
+    @visibility = data[:visibility]
   end
 
 end
