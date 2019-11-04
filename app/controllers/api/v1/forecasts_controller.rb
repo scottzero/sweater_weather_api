06becb8859e84d08dc89cc40b05141800323dbb3
Scott Payton <spayton.c@gmail.com>
2019-11-03 20:15:02 -0700

    remove comments

--- app/controllers/api/v1/forecasts_controller.rb
+++ app/controllers/api/v1/forecasts_controller.rb
@@ -1,19 +1,6 @@
 class Api::V1::ForecastsController < ApplicationController
 
   def show
-    # #need location
-    # location = params[:location]
-    # #location needs to be geocoded
-    # geocode_me = GeocodingService.new(location)
-    # #get data back from geocode service
-    # geocoded_location = geocode_me.parsed_google_geocode_data
-    # #location latitude and longitude
-    # lat_long_coords = geocode_me.lat_long_coords
-    # #get weather using darksky api
-    # weather = DarkskyService.new(lat_long_coords)
-    # binding.pry
-    #refactored
     weather_data = PresenterFacade.new(params[:location])
   end
-
 end
