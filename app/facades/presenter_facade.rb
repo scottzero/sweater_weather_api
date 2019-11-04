15cc81cb6f28cad0e0c05e8ebd7dcb23d8d4227a
Scott Payton <spayton.c@gmail.com>
2019-11-03 20:10:03 -0700

    Make presenter functional

--- app/facades/presenter_facade.rb
+++ app/facades/presenter_facade.rb
@@ -1,4 +1,21 @@
 class PresenterFacade
+  attr_reader :location
 
-
+  def initialize(location)
+    @location = location
+  end
+  #access to geocoding service
+  def google_geocode
+    geocode_access = GeocodingService.new(location)
+    send_me_to_darksky = geocode_access.lat_long_coords
+  end
+  #pass in our lat and long from google_geocode method
+  def darksky
+    dark_sky_access = DarkskyService.new(google_geocode)
+    send_me_to_poros = dark_sky_access.parsed_darksky_response_data
+  end
+  #method responsible for current weather data only
+  def current_weather
+    CurrentWeatherPoro.new(darksky[:currently])
+  end
 end
