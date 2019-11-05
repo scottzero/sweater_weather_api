9a516c49d753dbdbe7f400d2a3beecdf8937ba2f
Scott Payton <spayton.c@gmail.com>
2019-11-04 18:57:09 -0700

    static hourly solution for hourly weather

--- app/models/forecast_handler_poro.rb
+++ app/models/forecast_handler_poro.rb
@@ -13,7 +13,7 @@ class ForecastHandlerPoro
     @daily_data = daily_data
   end
 
-  def current_weather
+  def current_data
     {
     current_weather => @current_weather[:time],
     summary => @current_weather[:summary],
@@ -22,7 +22,21 @@ class ForecastHandlerPoro
     humidity => @current_weather[:humidity],
     uvindex => @current_weather[:uvindex],
     visibility => @current_weather[:visibility],
-    feelsLike => @current_weather[:apparentTempurature]
+    feelsLike => @current_weather[:apparentTempurature],
+    }
+  end
+
+  def daily_data
+    {
+
+    }
+  end
+
+  def hourly_data
+    {
+    @hourly_data[:data][0][:time] => @hourly_data[:data][0].values[5],
+    @hourly_data[:data][1][:time] => @hourly_data[:data][2].values[5],
+    @hourly_data[:data][2][:time] => @hourly_data[:data][3].values[5]
     }
   end
 end
