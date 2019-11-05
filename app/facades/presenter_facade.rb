f910bf887de249d17af19ac90c94bf02c97d5753
Scott Payton <spayton.c@gmail.com>
2019-11-04 19:49:45 -0700

    rendering json for weather in application controller

--- app/facades/presenter_facade.rb
+++ app/facades/presenter_facade.rb
@@ -10,7 +10,8 @@ class PresenterFacade
     send_me_to_darksky = geocode_access.lat_long_coords #gets us coords
     darksky(send_me_to_darksky)
   end
-  #pass in our lat and long from google_geocode method
+  #pass in our lat and long from google_geocode method and setup poro
+  #maybe break out into 1 more method to init forcast handler
   def darksky(coords)
     dark_sky_access = DarkskyService.new(coords)
     weather_data = dark_sky_access.parsed_darksky_response_data
