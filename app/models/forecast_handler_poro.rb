dd64fe2d53789b3655030465dd7a8327d330ab1d
Scott Payton <spayton.c@gmail.com>
2019-11-04 18:28:42 -0700

    create methods to handle data for different periods

--- app/models/forecast_handler_poro.rb
+++ app/models/forecast_handler_poro.rb
@@ -5,12 +5,13 @@ class ForecastHandlerPoro
               :daily_data
 
   def initialize(location,current_data,hourly_data,daily_data)
-    binding.pry
     @id = 1
     @location = location
     @current_data = current_data
     @hourly_data = hourly_data
     @daily_data = daily_data
   end
+
+  
 
 end
