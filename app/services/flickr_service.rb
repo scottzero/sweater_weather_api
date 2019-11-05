94495dd7bd859ba63b7922bbbbdbc6ae26184bdb
Scott Payton <spayton.c@gmail.com>
2019-11-04 20:50:24 -0700

    created a flickr service to get img data from flickr api...what a pain

--- app/services/flickr_service.rb
+++ app/services/flickr_service.rb
@@ -1,11 +1,14 @@
 class FlickrService
 
-  def conn
-
+  def conn(location)
+    conn = Faraday.new("https://api.flickr.com/services/rest/?method=flickr.photos.search&text=#{location}skyline&api_key=#{ENV['FLICKR_API_KEY']}&per_page=1&format=json&nojsoncallback=1") do |f|
+    f.adapter Faraday.default_adapter
+  end
   end
 
-  def get_my_img
-    
+  def img_data(location)
+    response = conn(location).get
+    JSON.parse(response.body, symbolize_names: true)
   end
 
 end
