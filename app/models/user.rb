4e5279997a83e9cf170730a6e0971b64d865f070
Scott Payton <spayton.c@gmail.com>
2019-11-05 15:08:32 -0700

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < AppicationRecord
+  has_secure_password
+  validates :email, uniqueness: true
+  after_create :gen_user_api_key
+
+  def gen_user_api_key
+    #need session api_key for user
+    #this will gen a random hex string
+    self.update(api_key: SecureRandom.hex)
+  end
+
+
+end
