ebe740270bae29f300e4853a98022034e8c721c5
Scott Payton <spayton.c@gmail.com>
2019-11-04 22:12:05 -0700

    modify forecast serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,4 +1,4 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :location, :current_data, :hourly_data
+    attributes :id, :location, :current_data, :hourly_data
 end
