bc1cceef2ce25ca5e8f1aa479f2bc1e030ce935b
Scott Payton <spayton.c@gmail.com>
2019-11-04 19:50:28 -0700

    modify forecasts controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+  def serialize_forecast
+    render json: ForecastSerializer.new(PresenterFacade.new(params[:location]).google_geocode)
+  end
 end
