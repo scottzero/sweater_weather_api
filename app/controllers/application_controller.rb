c0b5d20d077b5a004052309830d1baedfbee5be7
Scott Payton <spayton.c@gmail.com>
2019-11-04 21:08:50 -0700

    add serializer method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::API
   def serialize_forecast
     render json: ForecastSerializer.new(PresenterFacade.new(params[:location]).google_geocode)
   end
+
+  def serialize_background_img
+    render json: BackgroundSerializer.new(PresenterFacade.new(params[:location]).get_img(params[:location]))
+  end
 end
