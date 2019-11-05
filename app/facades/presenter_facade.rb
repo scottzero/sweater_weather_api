5d1d57a7ef63da131d89dcc28a5f835172e3acfe
Scott Payton <spayton.c@gmail.com>
2019-11-04 18:25:28 -0700

    refactor presenter

--- app/facades/presenter_facade.rb
+++ app/facades/presenter_facade.rb
@@ -4,23 +4,19 @@ class PresenterFacade
   def initialize(location)
     @location = location
   end
-  #access to geocoding service
+  #access to geocoding service, returns coordinates to darksky method
   def google_geocode
     geocode_access = GeocodingService.new(location)
-    send_me_to_darksky = geocode_access.lat_long_coords
-    binding.pry
+    send_me_to_darksky = geocode_access.lat_long_coords #gets us coords
+    darksky(send_me_to_darksky)
   end
   #pass in our lat and long from google_geocode method
-  def darksky
-    dark_sky_access = DarkskyService.new(google_geocode)
-    send_me_to_poros = dark_sky_access.parsed_darksky_response_data
-  end
-  #method responsible for current weather data only
-  def current_weather
-    CurrentWeatherPoro.new(darksky[:currently])
-  end
-  #method responsible for current daily data only
-  def daily_weather
-    DailyWeatherPoro.new(darksky[:daily])
+  def darksky(coords)
+    dark_sky_access = DarkskyService.new(coords)
+    weather_data = dark_sky_access.parsed_darksky_response_data
+    current_data = weather_data[:currently]
+    hourly_data = weather_data[:hourly]
+    daily_data = weather_data[:daily]
+    ForecastHandlerPoro.new(@location,current_data,hourly_data,daily_data)
   end
 end
