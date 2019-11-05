8750fbde20f35f1221dff8f6b29a009619fbed67
Scott Payton <spayton.c@gmail.com>
2019-11-05 15:38:54 -0700

    need to fix users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,16 @@
 class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
+    if user.save
+      render json: {api_key: user.api_key}, status: 201
+    else
+      render json: {error: "no user"}, status: 404
+      #working but need a user. 
+    end
   end
 
   private
-  def user_params
-    params.permit(:email, :password, :password_confirmation)
-  end
+    def user_params
+      params.permit(:email, :password, :password_confirmation)
+    end
 end
