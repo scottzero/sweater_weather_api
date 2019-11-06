93e661158368b240651ddae0985b14ec1826ac1e
Scott Payton <spayton.c@gmail.com>
2019-11-05 23:28:42 -0700

    Full coverage with stubbs

--- spec/requests/api/v1/forecast_request_spec.rb
+++ spec/requests/api/v1/forecast_request_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Consuming forcast api endpoint" do
+RSpec.describe "Test forecast endpoint" do
   it "returns the weather forecast data for a specific city" do
     json_response =  File.open("./app/fixtures/forecast_denver_api_call.json")
     WebMock.stub_request(:get, 'http://localhost:3000/api/v1/forecast?location=denver,co').
