70ed61bb5d32d23ad4711ff6f4defb0dfa13cb54
Scott Payton <spayton.c@gmail.com>
2019-11-05 19:29:54 -0700

    create endpoint for roadtrips

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get "/backgrounds", to: "backgrounds#show"
       post "/users", to: "users#create"
       post "/sessions", to: "sessions#create"
+      post "/road_trip", to: "roadtrip#create"
     end
   end
 end
